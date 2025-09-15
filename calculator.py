b9a1e47791ff8dfd87bf68df7bfd2faa42b3c93c
Rizky <riaadha.rizky@gmail.com>
2025-09-15 14:10:16 +0700

    feat: ubah jadi kalkulator menu interaktif

--- calculator.py
+++ calculator.py
@@ -1,14 +1,27 @@
+
+print("Kalkulator Sederhana")
+print("1. Penjumlahan")
+print("2. Pengurangan")
+print("3. Perkalian")
+print("4. Pembagian")
+print("5. Pangkat")
+print("6. Modulus")
+
+pilihan = int(input("Pilih operasi (1-6): "))
 a = int(input("Masukkan angka pertama: "))
 b = int(input("Masukkan angka kedua: "))
 
-print("Hasil penjumlahan:", a + b)
-print("Hasil pengurangan:", a - b)
-print("Hasil perkalian:", a * b)
-
-if b != 0:
-    print("Hasil pembagian:", a / b)
+if pilihan == 1:
+    print("Hasil:", a + b)
+elif pilihan == 2:
+    print("Hasil:", a - b)
+elif pilihan == 3:
+    print("Hasil:", a * b)
+elif pilihan == 4:
+    print("Hasil:", a / b if b != 0 else "Tidak bisa dibagi 0")
+elif pilihan == 5:
+    print("Hasil:", a ** b)
+elif pilihan == 6:
+    print("Hasil:", a % b)
 else:
-    print("Tidak bisa dibagi 0")
-
-print("Hasil pangkat:", a ** b)
-print("Hasil modulus:", a % b)
+    print("Pilihan tidak valid")
